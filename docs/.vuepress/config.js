59ee3f32e20f45a80b830dda932ea0857fa0d4fd
zhangrenhua <zhangrenhuaman@senses-ai.com>
2022-03-20 17:36:48 +0800

    删除datafan-ui前缀

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
   title: 'DataFan-UI',
   description: '所有的伟大，都源于一个勇敢的开始',
   logo: '/logo.png',
-  base: '/datafan-ui/',
+  base: '/',
   port: '8080',
   themeConfig: {
     // 配置顶部导航栏
