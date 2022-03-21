9c912f2852799eea89b0b569c6abaca1bf910248
刘俊 <793898892@qq.com>
2022-03-21 22:27:05 +0800

    ✨datafan-ui README.md

--- docs/.vuepress/nav/index.js
+++ docs/.vuepress/nav/index.js
@@ -1,5 +1,6 @@
 module.exports = [
   { text: '首页', link: '/' },
   { text: '规范文档', link: '/guide/' },
-  { text: '组件中心', link: '/components/' }
+  { text: '组件中心', link: '/components/' },
+  { text: 'GitHub', link: 'https://github.com/young-datafan/datafan-ui' }
 ]
