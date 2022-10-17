de9eb3a91718ee7f9c448b6a8aaf8641cbeeb9c7
刘俊 <793898892@qq.com>
2022-10-17 16:26:04 +0800

    🔧 nav调整

--- docs/.vuepress/nav/index.js
+++ docs/.vuepress/nav/index.js
@@ -1,7 +1,8 @@
 module.exports = [
   { text: '首页', link: '/' },
   { text: '组件中心', link: '/components/' },
-  { text: '规范文档', link: '/design/' },
-  { text: '开发手册', link: '/document/' },
-  { text: 'GitHub', link: 'https://github.com/young-datafan/datafan-ui' }
+  // { text: '规范文档', link: '/design/' },
+  // { text: '开发手册', link: '/document/' },
+  { text: '往期分享', link: '/document/' },
+  { text: 'GitHub', link: 'https://github.com/young-datafan/datafan-ui' },
 ]
